2411b18ad89672ed58af63245ccb9a3a0589a9fa
hata_win <mh.mundus@gmail.com>
2020-08-13 01:54:15 +0900

    8/13 Forum fin

--- test10_Forum/app.py
+++ test10_Forum/app.py
@@ -12,11 +12,11 @@ app.secret_key = 'U1sNMeUkZSuuX2Zn'
 def index():
     # ログインが必要
     if not bbs_login.is_login():
-        return redirect('/')
+        return redirect('/login')
     # ログ一覧を表示
     return render_template('index.html',
-                    user=bbs_login.get_user(),
-                    data=bbs_data.load_data())
+            user=bbs_login.get_user(),
+            data=bbs_data.load_data())
 
 # ログイン画面を表示
 @app.route('/login')
@@ -24,7 +24,7 @@ def login():
     return render_template('login.html')
 
 # ログイン処理
-@app.route('try_login', method=['POST'])
+@app.route('/try_login', methods=['POST'])
 def try_login():
     user = request.form.get('user', '')
     pw = request.form.get('pw', '')
@@ -33,3 +33,31 @@ def try_login():
         return redirect('/')
     # ログインに失敗したらメッセージを表示
     return show_msg('ログインに失敗しました')
+
+# ログアウト処理
+@app.route('/logout')
+def logout():
+    bbs_login.try_logout()
+    return show_msg('ログアウトしました')
+
+# 書き込み処理
+@app.route('/write', methods=['POST'])
+def write():
+    # ログインが必要
+    if not bbs_login.is_login():
+        return redirect('/login')
+    # フォームのテキストを取得
+    ta = request.form.get('ta', '')
+    if ta == '':
+        return show_msg('書き込みが空でした')
+    bbs_data.save_data_append(
+        user=bbs_login.get_user(),
+        text=ta)
+    return redirect('/')
+
+# テンプレートを利用してメッセージを出力
+def show_msg(msg):
+    return render_template('msg.html', msg=msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- test10_Forum/bbs_data.py
+++ test10_Forum/bbs_data.py
@@ -20,7 +20,7 @@ def save_data(data_list):
 def save_data_append(user, text):
     # レコードを用意
     tm = get_datetima_now()
-    data = {'name': user, 'text': text, 'data': tm}
+    data = {'name': user, 'text': text, 'date': tm}
     # 先頭にレコードを追記して保存
     data_list = load_data()
     data_list.insert(0, data)
@@ -29,4 +29,4 @@ def save_data_append(user, text):
 # 日時を文字列で得る
 def get_datetima_now():
     now = datetime.datetime.now()
-    return "{0:%Y%m%d %H:%M}".format(now)
+    return "{0:%Y/%m/%d %H:%M}".format(now)
